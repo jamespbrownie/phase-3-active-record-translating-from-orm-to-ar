eabe99e409d0f23ed7d2af26bd9eae125e56b728
James Brownie <jamespbrownie@gmail.com>
2021-11-05 12:48:08 -0500

    commit tree a17308ece6376acb7567124108765ff145024e6c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,22 @@
-class Dog
+# require "active_record"
+
+# #set up a database connection 
+# ActiveRecord::Base.establish_connection(
+#     adapter: "sqlite3",
+#     database: "db/test.sqlite3"
+# )
+
+# #create a students table
+# sql = <<-SQL
+#     CREATE TABLE IF NOT EXISTS dogs (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT
+#     )
+# SQL
+# ActiveRecord::Base.connection.execute(sql)
+
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
+
